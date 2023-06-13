7cef1088ee05ce5efafa2bc05edeea2adcbc3b13
Archie <a.rch1e@gmail.com>
2023-06-14 00:23:00 +0100

    Test

--- woodcutter/woodcutter.gradle.kts
+++ woodcutter/woodcutter.gradle.kts
@@ -1,4 +1,4 @@
-version = "0.0.3"
+version = "0.0.4"
 project.extra["PluginName"] = "Woodcutter" // This is the name that is used in the external plugin manager panel
 project.extra["PluginDescription"] = "An automatic woodcutter" // This is the description that is used in the external plugin manager panel
 
